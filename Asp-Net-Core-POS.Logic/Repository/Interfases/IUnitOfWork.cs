8dc27e2e277b402a3960d4d8552d37820479aa0c
Lidio Guedez <112365426lidioguedez@users.noreply.github.com>
2022-11-09 22:43:38 -0300

    Creando UnitOfWork

---
+++ Asp-Net-Core-POS.Logic/Repository/Interfases/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_POS.Logic.Repository.Interfases
+{
+    public interface IUnitOfWork
+    {
+        IBodegaRepository BodegaRepository { get; }
+        Task saveChangesAsinc();
+    }
+}
---
+++ Asp-Net-Core-POS.Logic/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Asp_Net_Core_POS.Logic.Data;
+using Asp_Net_Core_POS.Logic.Repository.Interfases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_POS.Logic.Repository
+{
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _context;
+        private IBodegaRepository _bodegaRepository;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IBodegaRepository BodegaRepository
+        {
+            get 
+            {
+                return _bodegaRepository = _bodegaRepository ?? new BodegaRepository(_context);
+            }
+        }
+
+        public async Task saveChangesAsinc()
+        {
+           await _context.SaveChangesAsync();
+        }
+    }
+}
